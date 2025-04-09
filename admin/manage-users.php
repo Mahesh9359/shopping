712b72b5449b09156fa700b5955ca41471bb1699
Mahesh9359 <maheshjadhav9359@gmail.com>
2025-04-10 04:48:03 +0530

    manage users page updated

--- admin/manage-users.php
+++ admin/manage-users.php
@@ -59,36 +59,39 @@ if (isset($_GET['del'])) {
 
                             <br />
 
-                            <table cellpadding="0" cellspacing="0" border="0" class="datatable-1 table table-bordered table-striped display" width="100%">
-                                <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Name</th>
-                                        <th>Email</th>
-                                        <th>Contact no</th>
-                                        <th>Shipping Address/City/State/Pincode</th>
-                                        <th>Billing Address/City/State/Pincode</th>
-                                        <th>Reg. Date</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
-                                    $query = mysqli_query($con, "SELECT * FROM users");
-                                    $cnt = 1;
-                                    while ($row = mysqli_fetch_array($query)) {
-                                    ?>
-                                    <tr>
-                                        <td><?php echo htmlentities($cnt); ?></td>
-                                        <td><?php echo htmlentities($row['name']); ?></td>
-                                        <td><?php echo htmlentities($row['email']); ?></td>
-                                        <td><?php echo htmlentities($row['contactno']); ?></td>
-                                        <td><?php echo htmlentities($row['shippingAddress'] . ", " . $row['shippingCity'] . ", " . $row['shippingState'] . "-" . $row['shippingPincode']); ?></td>
-                                        <td><?php echo htmlentities($row['billingAddress'] . ", " . $row['billingCity'] . ", " . $row['billingState'] . "-" . $row['billingPincode']); ?></td>
-                                        <td><?php echo htmlentities($row['regDate']); ?></td>
-                                    </tr>
-                                    <?php $cnt++; } ?>
-                                </tbody>
-                            </table>
+                            <!-- ✅ Added horizontal scroll wrapper -->
+                            <div style="overflow-x: auto;">
+                                <table cellpadding="0" cellspacing="0" border="0" class="datatable-1 table table-bordered table-striped display" width="100%">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Name</th>
+                                            <th>Email</th>
+                                            <th>Contact no</th>
+                                            <th>Shipping Address/City/State/Pincode</th>
+                                            <th>Billing Address/City/State/Pincode</th>
+                                            <th>Reg. Date</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $query = mysqli_query($con, "SELECT * FROM users");
+                                        $cnt = 1;
+                                        while ($row = mysqli_fetch_array($query)) {
+                                        ?>
+                                        <tr>
+                                            <td><?php echo htmlentities($cnt); ?></td>
+                                            <td><?php echo htmlentities($row['name']); ?></td>
+                                            <td><?php echo htmlentities($row['email']); ?></td>
+                                            <td><?php echo htmlentities($row['contactno']); ?></td>
+                                            <td><?php echo htmlentities($row['shippingAddress'] . ", " . $row['shippingCity'] . ", " . $row['shippingState'] . "-" . $row['shippingPincode']); ?></td>
+                                            <td><?php echo htmlentities($row['billingAddress'] . ", " . $row['billingCity'] . ", " . $row['billingState'] . "-" . $row['billingPincode']); ?></td>
+                                            <td><?php echo htmlentities($row['regDate']); ?></td>
+                                        </tr>
+                                        <?php $cnt++; } ?>
+                                    </tbody>
+                                </table>
+                            </div> <!-- ✅ End scroll wrapper -->
                         </div>
                     </div>						
                 </div><!--/.content-->
