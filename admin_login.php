f5aec2e903d389408924f7a12d0946d0b3ad6995
Mahesh9359 <maheshjadhav9359@gmail.com>
2025-04-10 03:41:53 +0530

    admin page updated

--- admin_login.php
+++ admin_login.php
@@ -26,7 +26,8 @@ if (isset($_POST['login'])) {
 <head>
     <meta charset="UTF-8">
     <title>Admin Login</title>
-    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
     <style>
         body {
             background: linear-gradient(135deg, #6e8efb, #a777e3);
