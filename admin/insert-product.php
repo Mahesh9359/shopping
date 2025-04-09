b6c86fc85f53b000ef597e8c817f6fbe164077c5
Mahesh9359 <maheshjadhav9359@gmail.com>
2025-04-10 02:25:56 +0530

    pending orders updated

--- admin/insert-product.php
+++ admin/insert-product.php
@@ -1,56 +1,62 @@
-
 <?php
+// ✅ Nothing should be before this line!
 session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 include('include/config.php');
-if(strlen($_SESSION['alogin'])==0)
-	{	
-header('location:index.php');
-}
-else{
-	
-if(isset($_POST['submit']))
-{
-	$category=$_POST['category'];
-	$subcat=$_POST['subcategory'];
-	$productname=$_POST['productName'];
-	$productcompany=$_POST['productCompany'];
-	$productprice=$_POST['productprice'];
-	$productpricebd=$_POST['productpricebd'];
-	$productdescription=$_POST['productDescription'];
-	$productscharge=$_POST['productShippingcharge'];
-	$productavailability=$_POST['productAvailability'];
-	$productimage1=$_FILES["productimage1"]["name"];
-	$productimage2=$_FILES["productimage2"]["name"];
-	$productimage3=$_FILES["productimage3"]["name"];
-//for getting product id
-$query=mysqli_query($con,"select max(id) as pid from products");
-	$result=mysqli_fetch_array($query);
-	 $productid=$result['pid']+1;
-	$dir="productimages/$productid";
-if(!is_dir($dir)){
-		mkdir("productimages/".$productid);
-	}
 
-	move_uploaded_file($_FILES["productimage1"]["tmp_name"],"productimages/$productid/".$_FILES["productimage1"]["name"]);
-	move_uploaded_file($_FILES["productimage2"]["tmp_name"],"productimages/$productid/".$_FILES["productimage2"]["name"]);
-	move_uploaded_file($_FILES["productimage3"]["tmp_name"],"productimages/$productid/".$_FILES["productimage3"]["name"]);
-$sql=mysqli_query($con,"insert into products(category,subCategory,productName,productCompany,productPrice,productDescription,shippingCharge,productAvailability,productImage1,productImage2,productImage3,productPriceBeforeDiscount) values('$category','$subcat','$productname','$productcompany','$productprice','$productdescription','$productscharge','$productavailability','$productimage1','$productimage2','$productimage3','$productpricebd')");
-$_SESSION['msg']="Product Inserted Successfully !!";
-
+// ✅ Secure session validation
+if (!isset($_SESSION['alogin']) || strlen($_SESSION['alogin']) == 0) {
+    header('location:index.php');
+    exit();
 }
 
+if (isset($_POST['submit'])) {
+    $category = $_POST['category'];
+    $subcat = $_POST['subcategory'];
+    $productname = $_POST['productName'];
+    $productcompany = $_POST['productCompany'];
+    $productprice = $_POST['productprice'];
+    $productpricebd = $_POST['productpricebd'];
+    $productdescription = $_POST['productDescription'];
+    $productscharge = $_POST['productShippingcharge'];
+    $productavailability = $_POST['productAvailability'];
 
+    $productimage1 = $_FILES["productimage1"]["name"];
+    $productimage2 = $_FILES["productimage2"]["name"];
+    $productimage3 = $_FILES["productimage3"]["name"];
+
+    // Get next product ID
+    $query = mysqli_query($con, "SELECT MAX(id) as pid FROM products");
+    $result = mysqli_fetch_array($query);
+    $productid = $result['pid'] + 1;
+    $dir = "productimages/$productid";
+
+    if (!is_dir($dir)) {
+        mkdir($dir, 0777, true);
+    }
+
+    move_uploaded_file($_FILES["productimage1"]["tmp_name"], "$dir/$productimage1");
+    move_uploaded_file($_FILES["productimage2"]["tmp_name"], "$dir/$productimage2");
+    move_uploaded_file($_FILES["productimage3"]["tmp_name"], "$dir/$productimage3");
+
+    $sql = mysqli_query($con, "INSERT INTO products(category, subCategory, productName, productCompany, productPrice, productDescription, shippingCharge, productAvailability, productImage1, productImage2, productImage3, productPriceBeforeDiscount) VALUES('$category', '$subcat', '$productname', '$productcompany', '$productprice', '$productdescription', '$productscharge', '$productavailability', '$productimage1', '$productimage2', '$productimage3', '$productpricebd')");
+
+    $_SESSION['msg'] = "Product Inserted Successfully !!";
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Admin| Insert Product</title>
-	<link type="text/css" href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<link type="text/css" href="/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
-	<link type="text/css" href="/css/theme.css" rel="stylesheet">
-	<link type="text/css" href="/images/icons/css/font-awesome.css" rel="stylesheet">
+	<link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+	<link type="text/css" href="css/theme.css" rel="stylesheet">
+	<link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
 	<link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600' rel='stylesheet'>
 <script src="http://js.nicedit.com/nicEdit-latest.js" type="text/javascript"></script>
 <script type="text/javascript">bkLib.onDomLoaded(nicEditors.allTextAreas);</script>
@@ -236,11 +242,11 @@ while($row=mysqli_fetch_array($query))
 
 <?php include('include/footer.php');?>
 
-	<script src="/scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
-	<script src="/scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
-	<script src="/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-	<script src="/scripts/flot/jquery.flot.js" type="text/javascript"></script>
-	<script src="/scripts/datatables/jquery.dataTables.js"></script>
+	<script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
+	<script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
+	<script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="scripts/flot/jquery.flot.js" type="text/javascript"></script>
+	<script src="scripts/datatables/jquery.dataTables.js"></script>
 	<script>
 		$(document).ready(function() {
 			$('.datatable-1').dataTable();
@@ -251,4 +257,4 @@ while($row=mysqli_fetch_array($query))
 		} );
 	</script>
 </body>
-<?php } ?>
+</html>
