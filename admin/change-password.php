6a6ea90b459af2f7dc12ccced1ca23947a8ea8b3
Mahesh9359 <maheshjadhav9359@gmail.com>
2025-04-10 01:28:39 +0530

    login.php updated

--- admin/change-password.php
+++ admin/change-password.php
@@ -40,10 +40,11 @@ if (isset($_POST['submit'])) {
     <meta charset="utf-8">
     <title>Admin | Change Password</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
-    <link href="/css/theme.css" rel="stylesheet">
-    <link href="/images/icons/css/font-awesome.css" rel="stylesheet">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+<link href="css/theme.css" rel="stylesheet">
+<link href="images/icons/css/font-awesome.css" rel="stylesheet">
+
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600" rel="stylesheet">
 
     <script type="text/javascript">
@@ -135,9 +136,10 @@ if (isset($_POST['submit'])) {
 
 <?php include('include/footer.php'); ?>
 
-<script src="/scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
-<script src="/scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
-<script src="/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="/scripts/flot/jquery.flot.js" type="text/javascript"></script>
+<script src="scripts/jquery-1.9.1.min.js"></script>
+<script src="scripts/jquery-ui-1.10.1.custom.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="scripts/flot/jquery.flot.js"></script>
+
 </body>
 </html>
